6850fe95bd974cc197a872f5953f0d7f3b23d1b2
jessburnett <jess.a.burnett@gmail.com>
2018-04-16 15:33:39 -0400

    cleanup js and html

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 //2018 Jessica Burnett
 //styles import
 import "./styles/main.scss";
-
 //js imports
 import {json, newsReq, status, dataPromise } from "./data";
 
-//fetch newsAPI data
+//generic functions for adding elements
+function createNode(element){
+    return document.createElement(element);
+}
 
+function append(parent, el){
+    return  parent.appendChild(el);
+}
+
+//fetch newsAPI data
 /*fetch() 1- won’t reject on HTTP error status instead
 /it will resolve normally (with ok status set to false),
  and it will only reject on network failure or if anything
@@ -17,31 +24,43 @@ fetch(newsReq)
     .then(status)
     .then(json)
     .then(function (data){
-        //log newsarticle titles
-        var newsArticles = data.articles;
+        const newsArticles = data.articles;
         for (let i = 0; i < newsArticles.length; i++){
-            //log newsarticle titles
-            var newsArticles = data.articles;
             for (let i = 0; i < newsArticles.length; i++){
-                let newsTitle = newsArticles[i].title;
-                let newsSource = newsArticles[i].source;
-                let sourceName = newsSource.name;
+                //elements to add html structure
+                let li = createNode('li'),
+                    h3 = createNode('h3'),
+                    a = createNode('a'),
+                    aSource = createNode('a'),
+                    pDesc = createNode('p')
+                ;
 
-                let h3 = document.createElement("h3");
-                let h5 = document.createElement("h5");
+                //article value assignments
+                let newsTitle = newsArticles[i].title,
+                    newsSource = newsArticles[i].source,
+                    sourceName = newsSource.name,
+                    newsDesc = newsArticles[i].description,
+                    sourceUrl = newsArticles[i].url
+                ;
 
-                let title = document.createTextNode(newsTitle);
-                let source = document.createTextNode(sourceName);
+
+                a.href = sourceUrl;
+                a.innerHTML = newsTitle;
+                pDesc.innerHTML = newsDesc;
+                aSource.href = sourceName;
+                aSource.innerHTML = sourceName;
 
 
-                h3.appendChild(title);
-                h5.appendChild(source);
-
-                let articles = document.getElementById("newsArticles");
-                articles.appendChild(h3).appendChild(h5);
+                const ul = document.getElementById("news__article");
+                append(h3, a);
+                append(li, h3);
+                append(pDesc, aSource);
+                append(li, pDesc);
+                append(ul, li);
             }
         }
         console.log('Request succeeded with JSON response', data);
-    }).catch(function(error){
-    console.log("Request failed error");
-});
+    })
+    .catch(function(error){
+        console.log(JSON.stringify(error));
+    });
